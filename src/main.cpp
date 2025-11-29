84acebd9ef2bcc239fdd6188a0ac2b89f7bb0a36
Xiao Di <552333302@qq.com>
2025-11-29 23:19:27 +0800

    修正了多文件方面的宏的缺失 修正了退出直接PostQuitMessage(0); 增加了位置记录

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 // 包含消息循环，作为入口
 
 #ifndef UNICODE
+// UNICODE宏可以让Windows函数自动的匹配到W版本
 #define UNICODE
-#endif 
+#endif
 
 #include <windows.h>
 #include <gdiplus.h>
@@ -15,57 +16,59 @@
 #include "../resource/resources.h"
 
 // 主程序
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow){
-    
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+
     // 保存hInstance到全局
     C_hInstance = hInstance;
 
-    int windowPositionX = GetPrivateProfileInt(L"record",L"win-x",100,L"./config.ini");
-    int windowPositionY = GetPrivateProfileInt(L"record",L"win-y",100,L"./config.ini");
-    
+    // 读取配置项文件中的record（上次退出时记录）部分
+    // win-x和win-y 上次退出时的窗口位置
+    int windowPositionX = GetPrivateProfileInt(L"record", L"win-x", 100, L"./config.ini");
+    int windowPositionY = GetPrivateProfileInt(L"record", L"win-y", 100, L"./config.ini");
+
     // 初始化COM库
     hrMain = CoInitializeEx(NULL,
-        // 单元线程，禁用OLE1
-        COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE
-    );
+                            // 单元线程，禁用OLE1
+                            COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
 
-    if(FAILED(hrMain)){
+    if (FAILED(hrMain))
+    {
         MessageBoxExW(
-            NULL,L"错误：00001，初始化COM库时发生异常，请检查系统相关文件是否完好",
-            L"KB - 运行时发生错误",MB_OK|MB_ICONEXCLAMATION,0
-        ); // 消息框提示出错
+            NULL, L"错误：00001，初始化COM库时发生异常，请检查系统相关文件是否完好",
+            L"KB - 运行时发生错误", MB_OK | MB_ICONEXCLAMATION, 0); // 消息框提示出错
         return 0;
     }
 
     // 注册窗口类
-    const wchar_t CLASS_NAME[]  = L"KeyBonk主窗口";
-    WNDCLASS wc = { }; // 用0初始化整个WindowClass
+    const wchar_t CLASS_NAME[] = L"KeyBonk主窗口";
+    WNDCLASS wc = {};            // 用0初始化整个WindowClass
     wc.lpfnWndProc = WindowProc; // 指定WindowProc函数
     wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME; // 窗口类名称
+    wc.lpszClassName = CLASS_NAME;              // 窗口类名称
     wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // 图标（其实意义不大）
-    RegisterClass(&wc); // 注册
+    RegisterClass(&wc);                         // 注册
 
     // 创建窗口
-    hwnd = CreateWindowEx(
+    hwnd = CreateWindowExW(
         WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW, // 支持透明，设置置顶，隐藏软件图标
-        CLASS_NAME, // 窗口类
-        L"KeyBonk主窗口", // 窗口文本
-        WS_POPUP | WS_VISIBLE | WS_CLIPCHILDREN, // 窗口风格
-		// 大小+位置
-        100, 100,200,200,
-        NULL,       // 父窗口   
-        NULL,       // 菜单
-        hInstance,  // 示例句柄
-        NULL        // 附带的软件数据
-	);
+        CLASS_NAME,                                       // 窗口类
+        L"KeyBonk主窗口",                                 // 窗口文本
+        WS_POPUP | WS_VISIBLE | WS_CLIPCHILDREN,          // 窗口风格
+                                                          // 大小+位置
+        windowPositionX, windowPositionY, 200, 200,
+        NULL,      // 父窗口
+        NULL,      // 菜单
+        hInstance, // 示例句柄
+        NULL       // 附带的软件数据
+    );
 
     // 创建失败则提示并返回，结束运行
-    if (hwnd == NULL){
+    if (hwnd == NULL)
+    {
         MessageBoxExW(
-            NULL,L"错误：00002，创建窗口时发生异常，请检查系统各项设置是否正常",
-            L"KB - 运行时发生错误",MB_OK|MB_ICONEXCLAMATION,0
-        ); // 消息框提示出错
+            NULL, L"错误：00002，创建窗口时发生异常，请检查系统各项设置是否正常",
+            L"KB - 运行时发生错误", MB_OK | MB_ICONEXCLAMATION, 0); // 消息框提示出错
         return 0;
     }
 
@@ -73,17 +76,17 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     COLORREF crKey = 13217535; // 偏粉红色的颜色
     SetLayeredWindowAttributes(hwnd, crKey, 0, LWA_COLORKEY);
 
-    ShowWindow(hwnd, nCmdShow); //展示窗口
+    ShowWindow(hwnd, nCmdShow); // 展示窗口
 
     // 初始化GDI+
     Gdiplus::GpStatus GDIpStatus; // 接收GDI+库的状态（错误码）
     Gdiplus::GdiplusStartupInput gdiplusStartupInput;
     GDIpStatus = Gdiplus::GdiplusStartup(&g_gdiplusToken, &gdiplusStartupInput, NULL);
-	if(GDIpStatus != Gdiplus::Ok){ // 如果存在问题
+    if (GDIpStatus != Gdiplus::Ok)
+    { // 如果存在问题
         MessageBoxExW(
-            NULL,L"错误：00003，初始化GDI+库时发生异常",
-            L"KB - 运行时发生错误",MB_OK|MB_ICONEXCLAMATION,0
-        ); // 消息框提示出错
+            NULL, L"错误：00003，初始化GDI+库时发生异常",
+            L"KB - 运行时发生错误", MB_OK | MB_ICONEXCLAMATION, 0); // 消息框提示出错
         return 0;
     }
     // 加载背景图片
@@ -96,18 +99,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         // 似乎也能用WH_KEYBOARD，但低级钩子用起来更稳定、简单些
         LowLevelKeyboardProc, // 传递回调函数地址
         GetModuleHandle(NULL),
-        0
-    );
-    if(KeyboardHook == NULL){ // 没拿到句柄则失败
+        0);
+    if (KeyboardHook == NULL)
+    { // 没拿到句柄则失败
         MessageBoxExW(
-            NULL,L"错误：00004，键盘钩子安装失败，请检查杀毒软件是否关闭",
-            L"KB - 运行时发生错误",MB_OK|MB_ICONEXCLAMATION,0
-        ); // 消息框提示出错
+            NULL, L"错误：00004，键盘钩子安装失败，请检查杀毒软件是否关闭",
+            L"KB - 运行时发生错误", MB_OK | MB_ICONEXCLAMATION, 0); // 消息框提示出错
     }
 
     // 消息循环
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,21 +1,29 @@
 // ===./src/utils.cpp===
 // 通用的工具函数实现
 
+#ifndef UNICODE
+// UNICODE宏可以让Windows函数自动的匹配到W版本
+#define UNICODE
+#endif
+
 #include <filesystem>
 #include <string>
 #include "../include/utils.h"
 
 // 判断文件是否存在
-bool FileExists(const wchar_t* rawPath){
-    std::wstring_view pathView{ rawPath };
+bool FileExists(const wchar_t *rawPath)
+{
+    std::wstring_view pathView{rawPath};
     return std::filesystem::exists(pathView);
 }
 
 // 资源的安全释放
-template<typename T>
-void safeRelease(T** resourcePointer){
+template <typename T>
+void safeRelease(T **resourcePointer)
+{
     // 检查是否为空
-    if(*resourcePointer){
+    if (*resourcePointer)
+    {
         // 释放资源
         (*resourcePointer)->Release();
         // 设置为空
