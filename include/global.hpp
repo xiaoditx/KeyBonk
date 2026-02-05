cd85b29e56d4a2f0bd638e712c485a23b0718e94
Xiao Di <121000027+xiaoditx@users.noreply.github.com>
2026-02-05 22:00:25 +0800

    [功能迭代][功能优化] 针对分层窗口的修整
    - 删除已经不再发挥作用的读入背景代码与配套的delete代码
    - 修改了设置窗口穿透时使用的透明度更改代码
    - 调整主窗口DC和背景位图为全局资源，程序运行结束才释放

--- include/global.hpp
+++ include/global.hpp
@@ -5,22 +5,24 @@
 #define GLOBAL_H
 
 #include <windows.h>
-#include <gdiplus.h>
 
 // 全局变量声明
-extern ULONG_PTR g_gdiplusToken;           // GDI+的token
-extern HWND hwnd;                          // 主窗口句柄
-extern HWND hwndAbout;                     // "关于"窗口句柄
-extern HWND hwndSetting;                   // “设置”窗口句柄
-extern Gdiplus::Image *g_pBackgroundImage; // 背景图片
-extern bool Mute;                          // 键盘是否静音
-extern bool MuteMouse;                     // 鼠标是否静音
-extern bool WindowPenetrate;               // 窗口穿透
-extern NOTIFYICONDATA nid;                 // 任务栏通知区域图标状态
-extern bool minimum;                       // 当前最小化状态
-extern wchar_t audioLibPath[MAX_PATH];     // 音频库位置
-extern HHOOK KeyboardHook; // 钩子句柄
-extern HHOOK MouseHook; // 钩子句柄
+extern ULONG_PTR g_gdiplusToken;       // GDI+的token
+extern HWND hwnd;                      // 主窗口句柄
+extern HWND hwndAbout;                 // "关于"窗口句柄
+extern HWND hwndSetting;               // “设置”窗口句柄
+extern bool Mute;                      // 键盘是否静音
+extern bool MuteMouse;                 // 鼠标是否静音
+extern bool WindowPenetrate;           // 窗口穿透
+extern NOTIFYICONDATA nid;             // 任务栏通知区域图标状态
+extern bool minimum;                   // 当前最小化状态
+extern wchar_t audioLibPath[MAX_PATH]; // 音频库位置
+extern HHOOK KeyboardHook;             // 钩子句柄
+extern HHOOK MouseHook;                // 钩子句柄
+extern HBITMAP hBmp;                   // 存储背景图片的位图
+extern HDC hdcScreen;                  // 主窗口屏幕DC
+extern HDC memDC;                      // 主窗口内存DC
+extern HBITMAP hOldBmp;                // 主窗口内存DC默认位图
 extern HINSTANCE C_hInstance;
 extern int C_nCmdShow;
 extern HRESULT hrMain; // 接受Windows函数的返回结果
