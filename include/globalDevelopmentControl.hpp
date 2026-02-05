1c09cb718d2c7a1c7866ca0bc505a2772ddfa3a5
Xiao Di <121000027+xiaoditx@users.noreply.github.com>
2026-02-06 01:30:43 +0800

    [版本号更新] 更新版本号为1.3.0.0

--- include/globalDevelopmentControl.hpp
+++ include/globalDevelopmentControl.hpp
@@ -2,8 +2,8 @@
 #define GLOBAL_DEVELOPMENT_CONTROL_HPP
 
 #define develop 1                                       // 开发控制开关，1为开发模式，0为发布模式
-#define rcVersion 1, 2, 4, 0                            // 资源版本号
-#define appVersion "1.2.4.0"                            // 版本号
+#define rcVersion 1, 3, 0, 0                            // 资源版本号
+#define appVersion "1.3.0.0"                            // 版本号
 #define w_appVersion TEXT(appVersion)                   // 宽字符的版本号
 #define versionChange L"背景图片支持带透明色的图像文件" // 本次更新的变化
 
